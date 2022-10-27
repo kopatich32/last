596cfb04d981fee32f723d092a1b613bbe6650d3
kopatich32 <kopatich1994@yandex.ru>
2022-10-27 16:39:19 +0300

    Первый пример

---
+++ local.my/practicle.php
@@ -0,0 +1,15 @@
+<?php
+class Man {
+   public $hairs = 'white';
+    public $body = 'ordinary';
+    public function eat ($calories){
+
+    }
+}
+//$Petya = new Man();
+//$Vasya = new Man();
+//
+//echo ('Petya has ' . $Petya->hairs . ' hairs color' . '<br>');
+//echo ('Vasya has ' . $Vasya->body . ' body' . '<br>');
+//$Vasya->hairs = 'black';
+//echo ('Petya has ' . $Vasya->hairs . ' hairs color' . '<br>');
