d6e3837dc43dee73620f565aa37a369eb9573159
kopatich32 <kopatich1994@yandex.ru>
2022-11-01 02:28:25 +0300

    last commit

--- local.my/practicle2.php
+++ local.my/practicle2.php
@@ -12,17 +12,29 @@
 
 //echo date('m.d.Y H:i',mktime(14,10,0,10,7,2018));
 //echo mt_rand();
-echo getrandmax() . '<br>';
-include 'practicle.php';
-function foo()
-{
-     global $color;
-     global $fruit;
-
-    include 'practicle.php';
-
-    echo "Одно $color $fruit";
-}
-
-echo "One $color $fruit";
-
+//echo getrandmax() . '<br>';
+//include 'practicle.php';
+//function foo()
+//{
+//     global $color;
+//     global $fruit;
+//
+//    include 'practicle.php';
+//
+//    echo "Одно $color $fruit";
+//}
+//
+//echo "One $color $fruit";
+ini_set('error_reporting', E_ALL);
+//$a = 123;
+//$b = 12.32543711;
+//$c = '12.3';
+//$d = "abcd";
+//if(is_numeric($d)){
+//    echo 'This is int';
+//}else {
+//    echo 'This is\'n int';
+//}
+//echo round($b,2);
+$num = 1234.56;
+echo number_format($num, 2 ,'.','  ');
